342afcdeeb2cbf7358d4d51fde1ed09735fc5938
victory212 <okoivictory2000@gmail.com>
2023-03-16 06:47:27 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+	
+	printf("%s\n", str);
+
+return (0);
+}
