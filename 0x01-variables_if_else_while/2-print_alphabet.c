17d9d1e828730ef2def9735a7eb71ac7354efbc9
victory212 <okoivictory2000@gmail.com>
2023-03-17 19:16:31 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all alphabets
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i <= 26; i++)
+{
+	putchar(alpha[i]);
+}
+	putchar('\n');
+return (0);
+}
