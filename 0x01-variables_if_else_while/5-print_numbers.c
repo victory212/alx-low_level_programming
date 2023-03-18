1851a032d1377f74e3903357f883047e9d444e87
victory212 <okoivictory2000@gmail.com>
2023-03-17 19:44:07 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	int num[] = {0,1,2,3,4,5,6,7,8,9};
+	int num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i;
 
 	for (i = 0; i <= 9; i++)
@@ -17,5 +17,5 @@ int main(void)
 	printf("%i", num[i]);
 }
 	printf("\n");
-return 0;
+return (0);
 }
