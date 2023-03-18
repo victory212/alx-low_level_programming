e8a2b319bbf2df845eb825e31ade744bdd29bd01
victory212 <okoivictory2000@gmail.com>
2023-03-17 19:40:27 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Printing Numbers of base 10
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int num[] = {0,1,2,3,4,5,6,7,8,9};
+	int i;
+
+	for (i = 0; i <= 9; i++)
+{
+	printf("%i", num[i]);
+}
+	printf("\n");
+return 0;
+}
