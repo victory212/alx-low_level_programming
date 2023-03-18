d37daf3c17178adb071d04a7f3474b8de6495e07
victory212 <okoivictory2000@gmail.com>
2023-03-17 19:30:31 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all alphabets except q and e
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char low[] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+{
+	putchar(low[i]);
+}
+	putchar('\n');
+return (0);
+}
