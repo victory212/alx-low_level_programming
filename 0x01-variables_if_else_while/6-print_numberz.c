78106eae31a031c33e8c2585b83fda3a2b981924
victory212 <okoivictory2000@gmail.com>
2023-03-17 19:52:05 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers without Printf
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+return (0);
+}
