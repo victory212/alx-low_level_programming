fe69206d9131a2c155f410b03865f4e82259f8c8
victory212 <okoivictory2000@gmail.com>
2023-03-17 20:15:01 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - possible combinations
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int  i;
+
+	for (i = 48; i < 58; i++)
+{
+	putchar(i);
+	if (i != 57)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
