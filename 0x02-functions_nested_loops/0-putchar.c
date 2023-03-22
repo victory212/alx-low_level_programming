59845aebf7869d22529fb2681dec6da8f4cb656b
victory212 <okoivictory2000@gmail.com>
2023-03-22 01:55:45 -0700

    task tast

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print putchar, char c
+ *
+ * Return: o
+ */
+
+int _putchar(char c);
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+return (0);
+}
