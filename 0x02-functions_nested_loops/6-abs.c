8825ef72461f3ce46f9bcebe963fb7a7c3b7d731
victory212 <okoivictory2000@gmail.com>
2023-03-22 04:17:51 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Program to conpute the abs value
+ * @r: type returned
+ * Return: the absolute value
+ */
+
+int _abs(int r)
+{
+
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
