7754b60afe77e7228d2a9c423c193aefe0678aec
victory212 <okoivictory2000@gmail.com>
2023-03-22 03:17:29 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - function to check for lowercase alphabets
+ * c - argument to check
+ *
+ * Return: 1 on sucess and 0 if not
+ *
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
