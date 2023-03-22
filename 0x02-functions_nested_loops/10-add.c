14bb41d8bfdb9ddccc2a25f78c35805134683035
victory212 <okoivictory2000@gmail.com>
2023-03-22 04:54:46 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - program to add two numbers
+ * @a: first num
+ * @b: second num
+ * Return: c
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * times_table - nine times table
+ * Return: 0
+ */
+
+void times_table(void)
+{
