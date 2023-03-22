a6a6dec521ada3275403f3c13d5ae320830127f6
victory212 <okoivictory2000@gmail.com>
2023-03-22 02:56:33 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print 10 times the alphabets
+ *
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char alpha;
+
+	while (i <= 10)
+{
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+	}
+}
+_putchar('\n');
+i++;
+}
