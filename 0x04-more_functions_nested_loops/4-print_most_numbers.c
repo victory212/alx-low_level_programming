6f9f9bf8204acff9961a65f965493551df1b4269
victory212 <okoivictory2000@gmail.com>
2023-03-23 16:54:16 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print numbers
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	char num[] = "01345679";
+
+	for (i = 0; i < 7; i++)
+	{
+	_putchar(num[i]);
+	}
+_putchar('\n');
+}
