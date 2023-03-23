bb2fd4a81af5946259593ba04690e549ebfb9187
victory212 <okoivictory2000@gmail.com>
2023-03-23 16:42:22 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
